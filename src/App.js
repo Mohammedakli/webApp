71e13593d9f0069173b9e0b3092e8aa20a701c1b
Momo <mohammedakli05@gmail.com>
2020-01-02 00:34:35 +0100

    list products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {
+  table: {
+    minWidth: 650,
+
+  },
 }
 
-export default App;
+export default class SimpleTable extends React.Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3000/api/list`)
+      .then(res => {
+        console.log("res.data", res.data)
+        const products = res.data;
+        this.setState({ products });
+      })
+  }
+  render() {
+
+    return (
+      <TableContainer component={Paper}>
+        <Table className={style.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell align="right">name</TableCell>
+              <TableCell align="right">type</TableCell>
+              <TableCell align="right">price</TableCell>
+              <TableCell align="right">rating</TableCell>
+              <TableCell align="right">warranty_years</TableCell>
+              <TableCell align="right">available</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.products.data && this.state.products.data.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row._id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.type}</TableCell>
+                <TableCell align="right">{row.price}</TableCell>
+                <TableCell align="right">{row.rating}</TableCell>
+                <TableCell align="right">{row.warranty_years}</TableCell>
+                <TableCell align="right">{row.available ? "true" : "false"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+
+  }
+
+}
